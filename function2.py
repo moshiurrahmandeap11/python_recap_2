d65109fdac850a981cdb92103a248dadd814555f
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-09-17 17:26:57 +0600

    Add max_of_three function to return the largest of three numbers

--- function2.py
+++ function2.py
@@ -16,3 +16,26 @@ os.system("cls")
 
 # Task 7: Ekta function likho max_of_three(a, b, c) jeta 3ta number er moddhe sobcheye boro ta return korbe (loop na, if-else use koro)।
 
+def max_of_three(a,b,c):
+    big = a
+
+    if b > big:
+        big = b
+    if c > big:
+        big = c
+    return big
+
+print(max_of_three(1,2,3))
+
+os.system("cls")
+
+
+
+# Task 8 (default parameter): Ekta function likho power(base, exponent=2) — jodi exponent na dao, default 2 (square) hishebe calculate hobe:
+
+
+def power(base, exponent=2):
+    total = base ** exponent
+    return total
+
+print(power(2, 3))

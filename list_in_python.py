718c4445a56e01dcbf1cb5ed5f8070f44db1c7df
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-09-17 17:40:16 +0600

    Enhance list_in_python.py with append and remove operations; update print statements for clarity

--- list_in_python.py
+++ list_in_python.py
@@ -4,3 +4,19 @@ print(fruits[1])
 
 # to print a list length
 print(len(fruits))
+
+# append something in a list
+append = fruits.append("orange")
+print(fruits)
+print(len(fruits))
+
+# remove something in a list
+remove = fruits.remove("apple")
+print(fruits)
+print(len(fruits))
+
+import os
+# os.system("cls")
+
+for fruit in fruits:
+    print(fruit)

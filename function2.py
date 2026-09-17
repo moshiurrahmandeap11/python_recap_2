94609bf02d63244554bffe48b484079b3d9d62dc
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-09-17 17:19:06 +0600

    Add factorial function and print statement; prepare for max_of_three function

--- function2.py
+++ function2.py
@@ -1,2 +1,18 @@
+import os,sys
+
 # Task 6: Ekta function likho factorial(n) jeta n! (factorial) calculate kore return korbe। (e.g., factorial(5) = 5×4×3×2×1 = 120)
+
+def factorial(n):
+    total = 1
+    for i in range(n,0, -1):
+        total = total * i
+    return total
+        
+
+print(factorial(5))
+
+
+os.system("cls")
+
+# Task 7: Ekta function likho max_of_three(a, b, c) jeta 3ta number er moddhe sobcheye boro ta return korbe (loop na, if-else use koro)।
 

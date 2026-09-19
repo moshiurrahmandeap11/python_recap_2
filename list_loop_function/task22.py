be952675f27e22d72a9077c258bd483d585e0e49
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-09-19 14:15:38 +0600

    Add function to return common elements from two lists without duplicates

---
+++ list_loop_function/task22.py
@@ -0,0 +1,10 @@
+# Task 22: Ekta function common_elements(list1, list2) likho jeta duita list e common (dutate e ache emon) value gulo notun list hishebe return korbe, duplicate chara
+
+list1 = [1,2,2,3]
+list2 = [4,5,5,6,6]
+
+def common_elements(list1, list2):
+    print(list1, list2)
+
+
+common_elements(list1, list2)
